dfa702246d654964aba0b48cdacd7bed532c1d2c
patrikstudent <patrik.ryden@student.nbi-handelsakademin.se>
2024-12-02 09:30:46 +0100

    Added Review

--- DatabaseConnection.cs
+++ DatabaseConnection.cs
@@ -6,7 +6,7 @@ public class DatabaseConnection
     private readonly string _host = "localhost";
     private readonly string _port = "5432";
     private readonly string _username = "postgres";
-    private readonly string _password = "postgres";
+    private readonly string _password = "ostmacka666";
     private readonly string _database = "postgres";
     
     private readonly NpgsqlDataSource _connection;
